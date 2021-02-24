bca271e0614ba98b9160ca7f626a4aaa3bb72dbd
juliawiktoria <maybe.nott123@gmail.com>
2021-02-24 16:32:13 +0000

    imgs apth

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ def presgan(dat, netG, netD, log_sigma, args):
 
         if epoch % args.save_imgs_every == 0:
             fake = netG(fixed_noise).detach()
-            path_to_imgs = args.results_folder + '/epoch%d'.format(epoch)
+            path_to_imgs = args.results_folder + '/epoch'+ str(epoch)
             # old
             # vutils.save_image(fake, '%s/presgan_%s_fake_epoch_%03d.png' % (args.results_folder, args.dataset, epoch), normalize=True, nrow=20) 
             for i in range(fake.size(0)): 
